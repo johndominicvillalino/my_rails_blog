ab3ba5c5dac9a0f5dc339672aa370dda5efc1025
JohnDy PC <johndominicvillalino@gmail.com>
2022-07-03 20:33:35 +0800

    add edit and update

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [ :show, :edit, :update, :destroy ]
 
 def index 
     @blogs = Blog.all
@@ -8,6 +9,13 @@ def new
     @blog = Blog.new
 end
 
+def show 
+end
+
+def edit
+end
+
+
 
 def create
     @blog = Blog.new(blog_params)
@@ -15,7 +23,7 @@ def create
     respond_to do |format|
 
         if @blog.save
-          format.html { redirect_to blogs_path, notice: "Entry was successfully created." }
+          format.html { redirect_to blogs_path, notice: "Blog was successfully created." }
         else
           format.html { render :new, status: :unprocessable_entity }
         end
@@ -30,15 +38,38 @@ def create
 
 end
 
-def show 
-    @blog = Blog.find(params["id"])
-    
+def destroy 
+  @blog.destroy
+  respond_to do |format|
+    format.html {redirect_to blogs_url, notice: "Blog was deleted"}
+  end
+  end
+
+def set_blog
+  @blog = Blog.find(params["id"])
 end
 
 
-def blog_params
-    params.require(:blog).permit(:author, :body, :title, :excerpt)
+
+def update
+  respond_to do |format|
+    if @blog.update(blog_params)
+      format.html { redirect_to blog_url(@blog), notice: "Friend was successfully updated." }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+    end
   end
+end
+
+private
+def set_blog
+  @blog = Blog.find(params[:id])
+end
+
+def blog_params
+  params.require(:blog).permit(:author, :title, :body, :excerpt)
+end
+
 
 
 
